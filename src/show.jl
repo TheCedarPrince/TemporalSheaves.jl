a1d20bb43acc553bed11d754037cd59be01bc540
TheCedarPrince <jacobszelko@gmail.com>
2026-04-02 19:45:54 -0400

    Stubbed out extension function

--- src/show.jl
+++ src/show.jl
@@ -1,11 +1,3 @@
-function display_in_repl(g::Graph)
-    print(Base.stdout, "Please install ImageInTerminal.jl and PNGFiles.jl:\n\n")
-
-    printstyled(Base.stdout, "using Pkg\n", color = :light_blue, bold = true)
-    printstyled(Base.stdout, "Pkg.add([\"ImageInTerminal\", \"PNGFiles\"])\n", color = :light_blue, bold = true)
-
-    println(Base.stdout, "\nThen TemporalSheavesTerminalViewExt will load and display_in_repl will work.\n")
-
-end
+function display_in_repl end
 
 export display_in_repl
