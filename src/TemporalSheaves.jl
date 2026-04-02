c6994d9bbd5fa24bbc99b2bbdeaef700b2eef20c
TheCedarPrince <jacobszelko@gmail.com>
2026-04-02 19:47:19 -0400

    Added includes for other functions, extension inits, and simplified

--- src/TemporalSheaves.jl
+++ src/TemporalSheaves.jl
@@ -1,44 +1,25 @@
 module TemporalSheaves
 
+    using Base: 
+        get_extension
+    using Base.Experimental: 
+        register_error_hint
     using Catlab
-        import Catlab.CategoricalAlgebra:
-          @acset_type,
-          @present,
-          FreeSchema,
-          codom,
-          dom,
-          elements,
-          representable,
-          subobject_classifier,
-          yoneda
-        import Catlab.Graphics:
-          to_graphviz
-        import Catlab.Graphics.Graphviz:
-          Graph
-        import Catlab.Models:
-          Presentation,
-          add_generators!,
-          generators
-        import Catlab.Subobjects:
-          Subobject,
-          ⟹
-        import Catlab.Theories:
-          Hom,
-          Ob
 
-    include("show.jl")
-
-    function op(p::Presentation)
-        op_p = Presentation(p.syntax)
-        add_generators!(op_p, generators(p, :Ob))
-        add_generators!(op_p, [Hom(nameof(h), codom(h), dom(h)) for h in generators(p, :Hom)])
-        if !isempty(generators(p, :Attr)) || !isempty(generators(p, :AttrType))
-          printstyled(Base.stdout, "This operation does not preserve attribute data.\n", color = :light_red, bold = true)
-          printstyled(Base.stdout, "Destroying detected attribute data; returning presentation without attribute information.\n", color = :light_red, bold = true)
+    function __init__()
+        register_error_hint(MethodError) do io, exc, argtypes, kwargs
+            if exc.f == display_in_repl
+                if isnothing(get_extension(TemporalSheaves, :TemporalSheavesTerminalViewExt))
+                    _extension_message("FileIO", display_in_repl, io)
+                    _extension_message("Sixel", display_in_repl, io)
+                end
+            end
         end
-        return op_p
     end
 
-    export op
-
+    include("utilities.jl")
+    include("structs.jl")
+    include("presentations.jl")
+    include("zigzag.jl")
+    include("show.jl")
 end
