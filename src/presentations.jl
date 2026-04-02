87c29cde970c66d9ca0a0cfffb3eaea1c4ca787f
TheCedarPrince <jacobszelko@gmail.com>
2026-04-02 19:47:57 -0400

    Wrote op method for presentation

---
+++ src/presentations.jl
@@ -0,0 +1,12 @@
+function op(p::Presentation)
+    op_p = Presentation(p.syntax)
+    add_generators!(op_p, generators(p, :Ob))
+    add_generators!(op_p, [Hom(nameof(h), codom(h), dom(h)) for h in generators(p, :Hom)])
+    if !isempty(generators(p, :Attr)) || !isempty(generators(p, :AttrType))
+      printstyled(Base.stdout, "This operation does not preserve attribute data.\n", color = :light_red, bold = true)
+      printstyled(Base.stdout, "Destroying detected attribute data; returning presentation without attribute information.\n", color = :light_red, bold = true)
+    end
+    return op_p
+end
+
+export op
