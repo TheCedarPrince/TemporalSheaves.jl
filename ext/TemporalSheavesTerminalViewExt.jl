d22036f76479c0b9427615f2c5003180dc19930c
TheCedarPrince <jacobszelko@gmail.com>
2026-04-02 19:45:27 -0400

    Added loading message and redid display_in_repl function with sixel

--- ext/TemporalSheavesTerminalViewExt.jl
+++ ext/TemporalSheavesTerminalViewExt.jl
@@ -1,18 +1,20 @@
 module TemporalSheavesTerminalViewExt
 
+    using Catlab.Graphics: to_graphviz
+    using Catlab.Graphics.Graphviz: Graph, run_graphviz
+    using FileIO: FileIO, load
+    using Sixel: Sixel, sixel_encode
     using TemporalSheaves
-    using ImageInTerminal
-    using PNGFiles
 
-    import Catlab.Graphics.Graphviz:
-        Graph,
-        run_graphviz
+    __init__() = @info "Terminal image preview for TemporalSheaves loaded!"
 
-    function display_in_repl(g::Graph)
-        buf = IOBuffer()
-        run_graphviz(buf, g, format="png")
-        seekstart(buf)
-        display(PNGFiles.load(buf))
+    function TemporalSheaves.display_in_repl(g::Graph; image_gap = true)
+        tmp = tempname() * ".png"
+        open(tmp, "w") do io
+            run_graphviz(io, g, format="png")
+        end
+        image_gap ? println("") : nothing
+        sixel_encode(stdout, load(tmp))
     end
 
 end
