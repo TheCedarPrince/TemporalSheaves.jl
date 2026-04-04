6230e3a1257bb8bdf9cde83e6e5218609aebf9d7
Documenter.jl <documenter@juliadocs.github.io>
2026-04-04 19:54:53 +0000

    build based on da9ca22

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TemporalSheaves","page":"Home","title":"TemporalSheaves","text":"Documentation for TemporalSheaves.\n\n","category":"section"}]
+}
