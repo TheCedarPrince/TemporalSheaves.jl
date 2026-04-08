86d3cb0eae3291d6a4624d9661f1dc69b3f4cd86
TheCedarPrince <jacobszelko@gmail.com>
2026-04-08 19:28:36 -0400

    Show method for multiple images in Documenter docs

---
+++ ext/TemporalSheavesHypertextExt.jl
@@ -0,0 +1,57 @@
+module TemporalSheavesHypertextExt
+
+using Catlab.Graphics
+using HypertextLiteral
+using TemporalSheaves
+
+__init__() = @info "Hypertext literal functions for TemporalSheaves loaded!"
+
+"""
+    to_graphviz_htl(graphs; node_labels=true, edge_labels=true,
+                    style="display: flex; flex-wrap: wrap;",
+                    graph_attrs=Dict(:splines=>"false", :rankdir=>"LR", :dpi=>"70"),
+                    kwargs...)
+
+Render a collection of Graphviz graphs as a single HypertextLiteral HTML
+object, suitable for display in Documenter.jl `@example` blocks.
+
+Each graph is converted to an SVG string and embedded as a raw HTML div,
+laid out according to the `style` keyword argument.
+
+# Example
+```julia
+subobs = subobject_graph_embedding(constructor)
+to_graphviz_htl(subobs)
+```
+"""
+function TemporalSheaves.to_graphviz_htl(
+        graphs;
+        style::String = "display: flex; flex-wrap: wrap;",
+        graph_attrs = Dict(:splines=>"false", :rankdir=>"LR", :dpi=>"70"),
+        kwargs...
+    )
+
+#= 
+
+Disclaimer: This code was inspired by the solution given here:
+https://github.com/JuliaPluto/Pluto.jl/discussions/2197#discussioncomment-3069437
+Many thanks!
+
+=#
+
+    plots = []
+    for g in graphs
+        g = to_graphviz(
+            g;
+            graph_attrs = graph_attrs,
+            kwargs...
+        )
+
+        svg = sprint(show, "image/svg+xml", g)
+        push!(plots, htl"<div>$(HTML(svg))</div>")
+    end
+
+    return htl"<div style=$style>$plots</div>"
+end
+
+end
--- src/show.jl
+++ src/show.jl
@@ -1,3 +1,6 @@
 function display_in_repl end
 
+function to_graphviz_htl end
+
 export display_in_repl
+export to_graphviz_htl
