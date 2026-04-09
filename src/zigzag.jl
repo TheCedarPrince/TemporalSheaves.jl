7a308af975c5696ccd91df4f8977d6f0427fa493
TheCedarPrince <jacobszelko@gmail.com>
2026-04-09 14:41:00 -0400

    Added draft docstrings

--- src/zigzag.jl
+++ src/zigzag.jl
@@ -1,7 +1,31 @@
 """
-Temp docstring
+    zigzag(time_points::Int; opposite::Bool = true)
+
+Generates a discrete representation of the discrete subunit interval category (referred to as the ZigZag category) for `n` time points.
+
+# Arguments
+
+- `time_points::Int` - how many time points to represent 
+
+# Keyword Arguments
+
+- `opposite::Bool` - if `true` make the underlying schema associated to the ZigZag category opposite; if `false`, the schema remains unchanged (default: `true`)
+
+# Returns
+
+- `sch::Presentation` - a `Presentation` representing the schema for `n` number of `time_points`
+
+- `cons::DataType` - an ACSet `DataType` that represents the constructor built by `@acset_type`
+
+# Example
+
+Create a ZigZag with 2 time points:
+
+```julia-repl
+julia> s, c = zigzag(2);
+```
 """
-function zigzag(time_points; opposite = true)
+function zigzag(time_points::Int; opposite::Bool = true)
 
     if opposite
         schema_name = Symbol("SchZop$(time_points)")
@@ -52,9 +76,34 @@ end
 
 
 """
-Temp docstring
+    zigzag!(time_points::Int; opposite::Bool = true)
+
+Generates a discrete representation of the discrete subunit interval category (referred to as the ZigZag category) for `n` time points prepared for combinatorial data.
+
+# Arguments
+
+- `time_points::Int` - how many time points to represent 
+
+# Keyword Arguments
+
+- `opposite::Bool` - if `true` make the underlying schema associated to the ZigZag category opposite; if `false`, the schema remains unchanged (default: `true`)
+
+# Returns
+
+- `sch::Presentation` - a `Presentation` representing the schema for `n` number of `time_points`
+
+- `cons::DataType` - an ACSet `DataType` that represents the constructor built by `@acset_type`
+
+# Example
+
+Create a ZigZag with 2 time points:
+
+```julia-repl
+julia> s, c = zigzag!(2);
+```
+
 """
-function zigzag!(time_points; opposite = true)
+function zigzag!(time_points::Int; opposite::Bool = true)
 
     if opposite
         schema_name = Symbol("SchZop$(time_points)!")
@@ -147,9 +196,45 @@ function zigzag!(time_points; opposite = true)
 end
 
 """
-Temp docs
+    get_timepoints(s::Presentation; type::Symbol = :discrete)
+
+Get time points associated to a given ZigZag schema.
+
+# Arguments
+
+- `s::Presentation` - a schema associated to a ZigZag schema 
+
+# Keyword Arguments
+
+- `type` - what time points to return (default: `:discrete`); options include:
+
+  - `:discrete` - the discrete time points associated to a schema 
+
+  - `:cumulative` - the cumulative time points associated to a schema 
+
+  - Anything else (like an empty string) returns all time points associated to a schema 
+
+# Returns
+
+- Vector of `Ob{:generator}` objects 
+
+# Example 
+
+```julia-repl
+julia> s, c = zigzag(2);
+
+julia> get_timepoints(s)
+2-element Vector{Catlab.Theories.FreeSchema.Ob{:generator}}:
+ t0
+ t1
+
+julia> get_timepoints(s, type = :cumulative)
+1-element Vector{Catlab.Theories.FreeSchema.Ob{:generator}}:
+ t0_to_t1
+```
+
 """
-function get_timepoints(s; type = :discrete)
+function get_timepoints(s::Presentation; type = :discrete)
 
     if type == :discrete
         return filter(s -> occursin(r"^t\d+$", string(s)), generators(s, :Ob))
@@ -163,9 +248,37 @@ end
 
 
 """
-Temp docs
+    subobject_graph_embedding(cons)
+
+Computes the subobjects associated to a ZigZag and interprets them as graph embeddings associated to the underlying graph of the constructor's schema.
+
+> **Note:** If you want raw subobjects, please see the `subobject_classifier` function.
+
+# Arguments 
+
+- `cons::DataType` - an ACSet `DataType` that represents the constructor built by `@acset_type`
+
+# Returns 
+
+- Vector of subobjects as graph embeddings (specifically, `NamedGraph`'s).
+
+# Example 
+
+Compute the subobjects on the ZigZag schema
+
+```julia-repl
+julia> s, c = zigzag(c);
+
+julia> subobs = subobject_graph_embedding(c);
+```
+
 """
 function subobject_graph_embedding(cons)
+    #=
+
+    TODO: Update this function to work only on schemas -- using the combinatorial nature of the zigzag, there is a general pattern we can exploit to simplify preparing the embeddings. We don't need to actually run subobject_classifier repeatedly. Furthermore, all we are using schema for is to get the objects and the mapping pattern. Good for a first draft, but could be significantly improved.
+
+    =#
     if !(cons <: AbstractZigZag)
         printstyled(Base.stdout, "\nConstructor is not an $(AbstractZigZag).\n", color = :light_red, bold = true)
         println(Base.stdout, "You can make one by: \n")
